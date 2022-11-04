710084f40d0bf3f50fb7ade2b889b13e216d8098
ArasModir <109582452+ArasModir@users.noreply.github.com>
2022-11-04 12:28:17 +0100

    Fetch API - Load Content from a JSON

--- js/app.js
+++ js/app.js
@@ -1,15 +1,32 @@
 document.getElementById('button1').addEventListener('click', loadTxt);
+document.getElementById('button2').addEventListener('click', loadJSON);
 
 function loadTxt() {
     fetch('data.txt')
-    .then(function(response){
-        return response.text();
-    })
-    .then(function(data){
-        console.log(data);
-        document.getElementById('result').innerHTML = data;
-    })
-    .catch(function(error){
-        console.log(error);
-    });
+        .then(function (response) {
+            return response.text();
+        })
+        .then(function (data) {
+            console.log(data);
+            document.getElementById('result').innerHTML = data;
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+}
+
+function loadJSON() {
+    fetch('employees.json')
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            let html = '';
+            data.forEach(function (employee) {
+                html += `
+                <li>${employee.name} ${employee.job}</li>
+                `;
+            });
+            document.getElementById('result').innerHTML = html;
+        })
 }
